8bbc895b93e823c4676e9c80cd8aa0460ef38bfb
Gokulakrishnan064 <gokulakrishn06@gmail.com>
2025-10-03 11:43:29 +0530

    Delete main directory

--- main/UglliNo.java
+++
@@ -1,8 +0,0 @@
-package main;
-import java.util.*;
-public class UglliNo {
-	public static void main(String[]args) {
-		Scanner sc=new Scanner(System.in);
-	}
-
-}
--- main/Value_with_sign.java
+++
@@ -1,9 +0,0 @@
-package main;
-import java.util.*;
-public class Value_with_sign {
-	public static void main(String[]args) {
-		Scanner re=new Scanner(System.in);
-		char a=re.next().charAt(0);
-		System.out.println((int )a);
-
-}}
